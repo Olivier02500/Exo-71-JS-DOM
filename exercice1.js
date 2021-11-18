8c3fb4e29d7d24156e7c9690675b045d1a1a931e
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-18 09:26:12 +0100

    make exo 71

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,17 @@
-let monDiv = .createElement('div');
+document.body.onload = addElement;
 
-document.getElementById('viewport').appendChild(monDiv);
+function addElement () {
 
+    let newDiv = document.createElement("div");
+    let  newContent = document.createTextNode('Ce div a été ajouté en utilisant javascript');
+    newDiv.appendChild(newContent);
+    let currentDiv = document.getElementById('div1');
+    document.body.insertBefore(newDiv, currentDiv);
+}
 
-
-
-
-
-
-
+let elem = document.getElementById('viewport')
+elem.style.backgroundColor = 'red';
+elem.style.width='100px';
+elem.style.height='100px';
+let elemto = document.getElementById('aSupprimer');
+elemto.parentElement.removeChild(aSupprimer);
